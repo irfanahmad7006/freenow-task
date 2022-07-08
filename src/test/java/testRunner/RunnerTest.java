f152d696cd7cd9ef4c007bbb33d20ce7bcb30a48
Irfan Ahmad <irfan.ahmad.22@gmail.com>
2022-07-08 15:53:35 +0530

    updated feature file path RunnerTest.java

--- src/test/java/testRunner/RunnerTest.java
+++ src/test/java/testRunner/RunnerTest.java
@@ -5,14 +5,12 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = {"src\\main\\resources\\featureFiles"},
+        features = {"src/main/resources/featureFiles"},
         glue = {"com.typicode.jsonplaceholder.stepDefinitions"}, //specify hooks location if not present in the same package as step defs
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","html:target/reports/cucumber.html"},
         monochrome = true,
         publish = true
 )
-
-//com/typicode/jsonplaceholder/stepDefinitions
 public class RunnerTest extends AbstractTestNGCucumberTests {
 }
 
